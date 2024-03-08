7a0baddaa6e11fabd6b97c288f6c162776a01762
Assendro <89154531+Assendro@users.noreply.github.com>
2024-03-08 03:36:44 +0300

    Update Key.jsx

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -4,7 +4,8 @@ import DateNow from "./DateNow";
 const password = 'Valantis';
 
 function Key() {
-  return md5(`${password}_${DateNow()}`) // я не знаю часового пояса сервера, так что в 0:00 могут быть 401 запросы из-за этого расхождения 
+  //return md5(`${password}_${DateNow()}`) // я не знаю часового пояса сервера, так что в 0:00 могут быть 401 запросы из-за этого расхождения 
+  return "6b6cb1e55175afd4a78c2831ba83c4b9"
 }
 
 
